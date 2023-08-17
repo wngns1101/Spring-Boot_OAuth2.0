47791a21cfa2be69c48c30b6d947c07ca6dce5a7
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 <wngns0812@gmail.com>
2023-08-17 15:39:58 +0900

    feat: 구글 resource 토큰 response 받는 로직 추가

---
+++ loginApiStudy/src/main/java/com/example/loginApiStudy/Api/GoogleJoinApi.java
@@ -0,0 +1,50 @@
+package com.example.loginApiStudy.Api;
+
+import com.example.loginApiStudy.DTO.GoogleLoginResponse;
+import com.example.loginApiStudy.DTO.GoogleTokenRequestDto;
+import com.example.loginApiStudy.Entity.User;
+import com.example.loginApiStudy.Service.GoogleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Controller
+@RequiredArgsConstructor
+public class GoogleJoinApi {
+    private final GoogleService googleService;
+
+    @GetMapping("/login/google")
+    public void googleLogin(@RequestParam(name = "code") String code) throws Exception {
+        System.out.println(code);
+
+        String accessToken = googleService.requestAccessToken(code);
+        System.out.println(accessToken);
+//        User user = googleService.requestResourceToken(accessToken);
+//        googleService.join(user);
+
+
+//        GoogleTokenRequestDto googleTokenRequestDto = GoogleTokenRequestDto.builder().clientId(GOOGLE_CLIENT_KEY).clientSecret(GOOGLE_SECRET_PW).code(code).redirectUri(GOOGLE_REDIRECT_URI).grantType("authorization_code").build();
+//        RestTemplate restTemplate = new RestTemplate();
+//        ResponseEntity<GoogleLoginResponse> apiResponse = restTemplate.postForEntity("https://oauth2.googleapis.com" + "/token", googleTokenRequestDto, GoogleLoginResponse.class);
+//        GoogleLoginResponse googleLoginResponse = apiResponse.getBody();
+//
+//        WebClient webClient =
+//
+//        System.out.println(googleLoginResponse.toString());
+//
+//        String googleToken = googleLoginResponse.getId_token();
+//
+//        //5.받은 토큰을 구글에 보내 유저정보를 얻는다.
+//        String requestUrl = UriComponentsBuilder.fromHttpUrl("https://oauth2.googleapis.com" + "/tokeninfo").queryParam("id_token",googleToken).toUriString();
+//
+//        String resultJson = restTemplate.getForObject(requestUrl, String.class);
+//        System.out.println(resultJson);
+    }
+}
--- loginApiStudy/src/main/java/com/example/loginApiStudy/Service/GoogleService.java
+++ loginApiStudy/src/main/java/com/example/loginApiStudy/Service/GoogleService.java
@@ -1,10 +1,57 @@
 package com.example.loginApiStudy.Service;
 
+import com.example.loginApiStudy.Entity.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 @Service
 public class GoogleService {
+
+    @Value("${google-client-id}")
+    private String GOOGLE_CLIENT_KEY;
+
+    @Value("${google-redirect-uri}")
+    private String GOOGLE_REDIRECT_URI;
+
+    @Value("${google-secret-pw}")
+    private String GOOGLE_SECRET_PW;
+
+    String authorizeURL = "https://oauth2.googleapis.com";
+    String resourceURL = "https://kapi.kakao.com";
+
     public void googleLogin(String code) {
         System.out.println("code = " + code);
     }
+
+    public String requestAccessToken(String code) throws Exception {
+        if(code == null) new Exception("Failed APi Call");
+        try {
+            WebClient webClient = WebClient.builder().baseUrl(authorizeURL).build();
+            String token = webClient.post()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/token")
+                            .queryParam("grant_type", "authorization_code")
+                            .queryParam("code", code)
+                            .queryParam("client_id", GOOGLE_CLIENT_KEY)
+                            .queryParam("client_secret", GOOGLE_SECRET_PW)
+                            .queryParam("redirect_uri", GOOGLE_REDIRECT_URI)
+                            .build())
+                    .retrieve()
+                    .bodyToMono(String.class)
+                    .block();
+            return token;
+        } catch (Exception e) {
+            throw new Exception("API call failed");
+        }
+    }
+
+//    public User requestResourceToken(String code) {
+//        User user = User.createUser()
+//    }
+//
+//    public void join(User user) {
+//
+//    }
 }
